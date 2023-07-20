7d1013b5806f72a8e8543a2f3fc6bd88be6754e1
Rossi21 <rossi21@gmail.com>
2023-07-20 07:35:29 -0700

    todos

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,10 +4,11 @@ import "./style.css";
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
-const InputField = ({ todo, setTodo }: Props) => {
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
   return (
-    <form className="input">
+    <form className="input" onSubmit={handleAdd}>
       <input
         type="input"
         value={todo}
